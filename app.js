c33e1522be17dded8fb69fdf7e692225482546ea
Cassio Izumi <izumicassio@gmail.com>
2022-10-30 18:17:35 -0300

    using express in app.js to listen to port 3000

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //Aqui vai o express para escutar a 'port'
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log(`Listening to port ${port}`);
+});
+
+app.get('/', (req, res)=> {
+    res.send('Home page /')
+})
