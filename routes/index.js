2b7401412718a51d5a86cd8d3388acd07da42674
Cassio Izumi <izumicassio@gmail.com>
2022-10-31 18:09:46 -0300

    Create directory controllers, models, routes and services.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+/* const controllers = require('./controller'); */
+
+const router = express.Router();
+
+/* router.post('/rota', controllers.exemplo); */
+
+export default router;
